33d770194ecd7e12c3c9261a75e84c02b637aec3
Jacopo Ghisolfi <jacopo.ghisolfi@gmail.com>
2014-05-09 18:03:25 +0200

    fixing the navbar collapse for mobile

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(".navbar-nav li a").click(function() {
 
 	$(this).parents("li").addClass("active");
 	$(".navbar-nav li").not($(this).parents("li")).removeClass("active");	
-})
+});
+
+$('.nav a').on('click', function(){
+    $(".navbar-toggle").click();
+});
